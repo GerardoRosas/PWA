1aed469272cba8fdcfcee487096c5ab49bcdb0fe
GerardoRosas <rosas.ruizlg@gmail.com>
2021-02-14 13:36:40 -0600

    Offline mode

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
+const nombre_cache = 'apv_v1';
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apb.js',
+    '/error.html'
+]
+
 //Cuando se instala el service worker
 //Este metodo install solo se ejecuta una sola vez
 self.addEventListener('install', e => {
     console.log('Instalando el service worker');
-    console.log(e);
+
+    e.waitUnitil(
+        caches.open(nombre_cache)
+            .then(cache => {
+                console.log('cacheando');
+                cache.addAll(archivos);
+            })
+    )
 })
 
 
@@ -18,4 +36,12 @@ self.addEventListener('activate', e => {
 //Evento fetch para descargar archivos estaticos
 self.addEventListener('fetch', e => {
     console.log('Fetch', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then(respuestaCache => {
+                return respuestaCache
+            })
+            .catch(() => caches.match('/error.html'))
+    )
 })
